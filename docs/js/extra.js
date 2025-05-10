0b72848a9967517a73ede9cd1600f8a7ad7f65ab
LuciMoore <lmoore@umn.edu>
2025-05-10 14:24:55 -0700

    js

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,16 +1,13 @@
-// Function to make embedded links open new tab when clicked
-document.addEventListener('DOMContentLoaded', function() {
-  // Select all anchor tags with href starting with "http"
-  const externalLinks = document.querySelectorAll('a[href^="http"]:not([target="_blank"])');
-  
-  externalLinks.forEach(function(link) {
-      link.setAttribute('target', '_blank'); 
-      link.setAttribute('rel', 'noopener noreferrer');
-  });
-});
-
-// Mermaid enable elk layout
+// Mermaid diagram formatting
 document.addEventListener("DOMContentLoaded", function () {
+  // Open external links in new tab
+  const externalLinks = document.querySelectorAll('a[href^="http"]:not([target="_blank"])');
+  externalLinks.forEach(function(link) {
+    link.setAttribute('target', '_blank'); 
+    link.setAttribute('rel', 'noopener noreferrer');
+  });
+
+  // Initialize Mermaid with ELK layout
   mermaid.initialize({
     startOnLoad: true,
     theme: "default",
